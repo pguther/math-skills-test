48b31e933c6e19fea5dedba5f7d8f1fe3f819d01
Philip Guther <philip@guther.com>
2017-12-02 14:26:48 -0800

    dummy code for displaying problems

---
+++ app/static/js/math-test.js
@@ -0,0 +1,62 @@
+var currentIndex = 0;
+var questions;
+
+$(document).ready(function(){
+
+
+    questions = [
+        {
+            "equation" : "5 + 1",
+            "answer" : 6,
+            "userInput" : ""
+        },
+        {
+            "equation" : "2 + 2",
+            "answer" : 4,
+            "userInput" : ""
+        },
+        {
+            "equation" : "3 - 1",
+            "answer" : 2,
+            "userInput" : ""
+        }
+    ];
+
+    $("#equation").text(questions[currentIndex]["equation"]);
+
+    var done = function() {
+        debugger;
+    }
+
+    var submitAnswer = function() {
+        if (currentIndex < questions.length - 1) {
+            questions[currentIndex]["userInput"] = $("#answer").val();
+            $("#answer").val("");
+            currentIndex = currentIndex + 1;
+            $("#equation").text(questions[currentIndex]["equation"]);
+            $("#answer").focus();
+        } else {
+            questions[currentIndex]["userInput"] = $("#answer").val();
+            $("#answer").val("");
+            $("#equation").text("done");
+            $("#answer").focus();
+            done();
+        }
+
+
+
+    }
+
+    $("#next").click(function(){
+        submitAnswer();
+    });
+
+    $("#answer").keyup(function(event) {
+        var code = (event.keyCode ? event.keyCode : event.which);
+        if(code == 13) { //Enter keycode
+            submitAnswer();
+        }
+    });
+
+
+});
--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
 from app import app
 from flask import render_template
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
